233dc4a1776512cf37c0fbebbe1f11c290566672
fonsecapamela <p4m3l4.c05t4@gmail.com>
2024-02-15 19:24:21 -0300

    JavaScript

---
+++ JS_descomplica/script1.js
@@ -0,0 +1,4 @@
+let nome = 'Pamela Fonseca'
+let idade = '27'
+
+console.log("seu nome é: "+nome + "e sua idade é" + idade)
---
+++ JS_descomplica/script2.js
@@ -0,0 +1,3 @@
+let rlSync - requestAnimationFrame('redline-sync')
+let nome = rlSync.question ('Qual é o seu nome?')
+console.log(`Olá, ${nome}`)
